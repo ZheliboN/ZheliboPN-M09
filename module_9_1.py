2eb619e304537f51e6fcfa715ec3b71bd1b89922
ZheliboN <zhelibo_pavel@mail.ru>
2024-09-15 11:37:22 +0800

    Домашнее задание по теме "Введение в функциональное программирование". Добавлено преобразование исходного списка в список чисел INT, при попытке выполнения со строками

--- module_9_1.py
+++ module_9_1.py
@@ -1,5 +1,6 @@
 def apply_all_func(int_list, *functions):
     results = {}
+    int_list = list(map(int, int_list))
     for func in functions:
         results[func.__name__] = func(int_list)
     return results
@@ -7,3 +8,4 @@ def apply_all_func(int_list, *functions):
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+print(apply_all_func(['6', '20', '15', '9'], len, sum, sorted))
