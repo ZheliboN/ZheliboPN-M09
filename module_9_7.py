2daa866ddf4a2ecd82c0949e5dfa4a6a352163a7
ZheliboN <zhelibo_pavel@mail.ru>
2024-09-23 20:11:24 +0800

    Доработанное по замечанию домашнее задание по теме "Декораторы".

--- module_9_7.py
+++ module_9_7.py
@@ -1,21 +1,22 @@
 
 def is_prime(function):
     def wrapper(*args):
-        prime_number = True
         res = function(*args)
-        if res < 2:
-            prime_number = False
-        else:
-            for i in range(2, res):
-                if res % i == 0:
-                    prime_number = False
-                    break
-                else:
-                    prime_number = True
-        if prime_number:
-            print('Простое')
-        else:
-            print('Составное')
+        if res > 0:
+            prime_number = True
+            if res < 2:
+                prime_number = True
+            else:
+                for i in range(2, res):
+                    if res % i == 0:
+                        prime_number = False
+                        break
+                    else:
+                        prime_number = True
+            if prime_number:
+                print('Простое')
+            else:
+                print('Составное')
         return res
     return wrapper
 
@@ -33,4 +34,7 @@ result = sum_three(9, 8, 7)
 print(result)
 
 result = sum_three(8, 11, 4)
+print(result)
+
+result = sum_three(0, 1, 0)
 print(result)
